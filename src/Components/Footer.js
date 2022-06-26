0c9edff9e23c22cfcd6d531941947aded3d102b6
Harsh <harsharya7002@gmail.com>
2022-06-27 00:27:31 +0530

    creating footer and adding css with the help of object

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,18 @@
  import React from 'react'
  
  function Footer() {
+     let footerStyle = {
+         position: "absolute",
+         top: "92vh",
+         width: "100%",
+         border: "2px solid red"
+     }
    return (
-     <div>Footer</div>
+     <div className='bg-dark text-light py-2' style={footerStyle}>
+         <p className='text-center'>
+         Copyright &copy; MyTodo.com
+         </p>
+         </div>
    )
  }
  
