12f5f00f0f4bd86984ad175d91aab8f184cc0340
Harsh <harsharya7002@gmail.com>
2022-06-27 17:06:39 +0530

    add useState to the form

--- src/Components/AddToDo.js
+++ src/Components/AddToDo.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const AddToDo = () => {
+const AddToDo = (props) => {
+    const [title, setTitle] = useState("")
+    const [desc, setDesc] = useState("")
+    const submit = () =>{
+
+    }
   return (
     <div className='container my-3 text-center'>
         <h3>Add a Task</h3>
-        <form>
+        <form onSubmit={submit}>
   <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Task To Do</label>
-    <input type="text" class="form-control" id="exampleInputEmail1"/>
+    <label for="title" class="form-label">Task Title</label>
+    <input type="text" class="form-control" value={title} id="title"/>
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Add a Description</label>
-    <input type="text" class="form-control" id="exampleInputPassword1"/>
+    <label for="desc" class="form-label">Add a Description</label>
+    <input type="text" class="form-control" value={desc} id="desc"/>
   </div>
   <button type="submit" class="btn btn-success btn-sm">Add ToDo</button>
 </form>
